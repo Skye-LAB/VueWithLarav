40133002184b8904d282ab24cd4a6673b99a30c6
Xazkiel0 <xazkiel@gmail.com>
2020-02-14 10:34:13 +0700

    Msg[ignore]

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
-Route::get('/admin', function () {
-    return view('admin');
-});
+Route::get('/admin', 'HomeController@admin');
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('/admin/menu', 'MenuController');
     Route::resource('/admin/employee', 'EmployeeController');
